7cbf97242bf520d304ebbdf03d29b939250f8a68
Harsh Pathak <harphy45@gmail.com>
2026-09-05 00:24:51 +0530

    Update App to use useLists hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
-import { useState, useEffect, useCallback } from 'react'
 import ProjectSidebar from './components/ProjectSidebar'
 import TaskList from './components/TaskList'
 import TitleBar from './components/TitleBar'
-
-const api = window.taskmaster
+import { useLists } from './hooks/useLists'
 
 export default function App() {
-  const [lists, setLists] = useState([])
-  const [activeList, setActiveList] = useState(null)
-
-  const loadLists = useCallback(async () => {
-    const data = await api.getLists()
-    setLists(data)
-    if (!activeList && data.length > 0) setActiveList(data[0])
-  }, [activeList])
-
-  useEffect(() => {
-    loadLists()
-    api.onTasksUpdated(() => loadLists())
-  }, [])
-
-  const handleCreateList = async (name, color) => {
-    const newList = await api.createList(name, color)
-    setLists((prev) => [...prev, newList])
-    setActiveList(newList)
-  }
-
-  const handleDeleteList = async (id) => {
-    await api.deleteList(id)
-    setLists((prev) => {
-      const updated = prev.filter((l) => l.id !== id)
-      if (activeList?.id === id) setActiveList(updated[0] || null)
-      return updated
-    })
-  }
+  const { lists, activeList, setActiveList, createList, deleteList } = useLists();
 
   return (
     <div className="app-layout">
@@ -43,8 +14,8 @@ export default function App() {
           lists={lists}
           activeList={activeList}
           onSelectList={setActiveList}
-          onCreateList={handleCreateList}
-          onDeleteList={handleDeleteList}
+          onCreateList={createList}
+          onDeleteList={deleteList}
         />
         {activeList ? (
           <TaskList key={activeList.id} list={activeList} />
